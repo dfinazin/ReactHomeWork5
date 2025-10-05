a298e5fb5e22137b219374793cc9b25be056ee53
dfinazhin <dfinazhin@gmail.com>
2025-10-06 00:34:35 +0500

    Init

---
+++ README.md
@@ -0,0 +1,4 @@
+# React home work 5
+
+Модуль 2. Frontend 5. Запросы к серверу
+ResultSchool FrontendMiddle React Домашка №5
---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import styles from './app.module.css';
+export const App = () => {
+    return (
+        <>
+            <div className={styles.App}>
+                <div></div>
+            </div>
+        </>
+    );
+};
