c09dbb9e14b6379d94a54444ec9f79443ff28963
dfinazhin <dfinazhin@gmail.com>
2025-10-06 01:50:56 +0500

    Запросы к серверу useEffect часть 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,39 @@
+import { useEffect, useState } from 'react';
 import styles from './app.module.css';
+
 export const App = () => {
+    const [products, setProducts] = useState([]);
+
+    const loadProducts = async () => {
+        try {
+            const responce = await fetch(
+                'https://mocki.io/v1/1f5c883c-5f92-4852-9b5a-8d5e836f6bb3',
+            );
+            if (!responce.ok) {
+                throw new Error('Список товаров получить не удалось');
+            } else {
+                const loadedData = await responce.json();
+                setProducts(loadedData);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        loadProducts();
+    }, []);
+
     return (
         <>
-            <div className={styles.App}>
-                <div></div>
+            <div className={styles.app}>
+                {products.map(({ id, name, price }) => {
+                    return (
+                        <div key={id}>
+                            {name} - {price} руб
+                        </div>
+                    );
+                })}
             </div>
         </>
     );
